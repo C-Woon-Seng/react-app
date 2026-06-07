f84acfb900239eb1af30e469efd2c002935a2356
Woon Seng <241349E@mymail.nyp.edu.sg>
2026-06-07 12:36:54 +0800

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
 import NavBar from "./components/NavBar"
+import AuthProvider from "./context/AuthContext";
 
 function App() {
-  return <div className="app">
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/auth" element={<Auth/>}/>
-      <Route path="/checkout" element={<Checkout/>}/>
-    </Routes>
-  </div>
+  return (
+    <AuthProvider>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
-export default function ProductCard( product ) {
+export default function ProductCard({ product }) {
     return (
-        <div className="product-card">
+        <div className="product-card" >
             <img src={product.image} className="product-card-image" />
             <div className="product-card-content">
                 <h3 className="product-card-name">{product.name}</h3>
                 <p className="product-card-price">${product.price}</p>
                 <div className="product-card-actions">
-                    <Link className="btn btn-secondary">View details</Link>
+                    <Link to={`/product/${product.id}`} className="btn btn-secondary">View details</Link>
                     <button className="btn btn-primary">Add to Cart</button>
                 </div>
             </div>
